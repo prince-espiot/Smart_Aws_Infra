75f99ca72ef73510badcccb2859d1e43d3161357
prince-espiot <okumop@yahoo.com>
2025-01-10 17:06:46 +0200

    Argocd and image updater added

--- observability_n_gitops/gitops-operation-argo.tf
+++ observability_n_gitops/gitops-operation-argo.tf
@@ -1,12 +1,15 @@
-/*resource "helm_release" "argo_cd" {
+resource "helm_release" "argo_cd" {
   name       = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   namespace  = "argocd"
+  version    = "7.7.15"
 
   create_namespace = true
 
-
+  values = [
+    file("${path.module}/../values/argocd.yaml"),
+  ]
 }
-*/
+
 
